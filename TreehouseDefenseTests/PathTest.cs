7ad9b2765aa09072b61e00ff85765c7edc45a452
lduenas <luis.alonso.duenas.m@gmail.com>
2018-12-28 10:32:56 -0800

    fixed of-by-one in Path.GetLocationAt

--- TreehouseDefenseTests/PathTest.cs
+++ TreehouseDefenseTests/PathTest.cs
@@ -1,23 +1,26 @@
-﻿using TreehouseDefense;
+﻿using System.Linq;
+using TreehouseDefense;
 using Xunit;
 
 namespace TreehouseDefenseTests
 {
-    public class PathTest
+    public class PathTests
     {
         private Map _map3x3;
         private MapLocation[] _pathLocations3;
         private Path _path3;
 
-        public PathTest()
+        public PathTests()
         {
             _map3x3 = new Map(3, 3);
-             _pathLocations3 = new MapLocation[]
+
+            _pathLocations3 = new MapLocation[]
             {
-                new MapLocation(0,1,_map3x3),
-                new MapLocation(1,1,_map3x3),
-                new MapLocation(2,1,_map3x3)
+                new MapLocation(0, 1, _map3x3),
+                new MapLocation(1, 1, _map3x3),
+                new MapLocation(2, 1, _map3x3)
             };
+
             _path3 = new Path(_pathLocations3);
         }
 
@@ -25,7 +28,6 @@ namespace TreehouseDefenseTests
         public void MapLocationIsOnPath()
         {
             var target = _path3;
-
             Assert.True(target.IsOnPath(new MapLocation(0, 1, _map3x3)));
         }
 
@@ -33,8 +35,35 @@ namespace TreehouseDefenseTests
         public void MapLocationIsNotOnPath()
         {
             var target = _path3;
-
             Assert.False(target.IsOnPath(new MapLocation(0, 0, _map3x3)));
+        }
+
+        [Fact]
+        public void GetLocationAtBeginningOfPath()
+        {
+            var target = _path3;
+            Assert.Equal(_pathLocations3.First(), target.GetLocationAt(0));
+        }
+
+        [Fact]
+        public void GetLocationAtEndOfPath()
+        {
+            var target = _path3;
+            Assert.Equal(_pathLocations3.Last(), target.GetLocationAt(_pathLocations3.Length - 1));
+        }
+
+        [Fact]
+        public void GetLocationNotOnPath()
+        {
+            var target = _path3;
+            Assert.Null(target.GetLocationAt(_pathLocations3.Length + 1));
+        }
+
+        [Fact]
+        public void GetLocationAtOneStepAfterEndOfPath()
+        {
+            var target = _path3;
+            Assert.Null(target.GetLocationAt(_pathLocations3.Length));
         }
     }
 }

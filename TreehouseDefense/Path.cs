667c3b592a42fa8461626557eccdfcccd70ee044
lduenas <luis.alonso.duenas.m@gmail.com>
2018-12-28 10:25:30 -0800

    Add project files.

---
+++ TreehouseDefense/Path.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+namespace TreehouseDefense
+{
+    public class Path
+    {
+        private MapLocation[] _path;
+
+        public Path(MapLocation[] pathLocations)
+        {
+            _path = pathLocations;
+        }
+
+        public bool IsOnPath(MapLocation mapLocation) => _path.Contains(mapLocation);
+    }
+}
